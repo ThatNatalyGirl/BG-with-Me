6197c86526f46e45d4968e67de380a2913524c8e
Nataly <ncgarzon88@gmail.com>
2018-08-18 23:25:05 -0400

    google maps works

---
+++ frontend/dist/js/googleMaps.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log("I'm a map");
+
+var map;
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: -34.397, lng: 150.644 },
+		zoom: 8
+	});
+}
+//# sourceMappingURL=googleMaps.js.map
